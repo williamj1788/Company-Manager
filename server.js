eeb9d89d336a0cbf02833f1316f368d312f46b3d
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-06 16:41:59 -0500

    created fetch request to server

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const path = require('path');
+// const bodyParser = require('body-parser');
+const multer = require('multer');
+var upload = multer();
 
 var app = express();
 
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+app.get('/signup', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public/signup.html'));
+})
+app.post('/login', upload.none(),(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/company');
+})
+app.get('/company', upload.none(),(req, res, next) => {
+    console.log('served');
+    res.sendFile(path.join(__dirname, 'public/company.html'));
 })
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}...`)})
