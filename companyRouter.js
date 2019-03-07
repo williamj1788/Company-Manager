d3b4a180d7424ca7f532bc9b8afa8d7304f1b272
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-07 05:40:37 -0500

    add routing from company page to company info

--- companyRouter.js
+++ companyRouter.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', (req,res,next) => {
     res.sendFile(path.join(__dirname, 'public/company.html'));
 });
+router.get('/customers', (req,res,next) => {
+    res.sendFile(path.join(__dirname, 'public/customer.html'));
+});
+router.get('/products', (req,res,next) => {
+    res.sendFile(path.join(__dirname, 'public/product.html'));
+});
+router.get('/stores', (req,res,next) => {
+    res.sendFile(path.join(__dirname, 'public/store.html'));
+});
 
 module.exports = router;
