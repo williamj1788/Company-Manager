cdad586dd239fe03f2b397da776bc50f619ae674
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-03-08 03:43:43 -0500

    customers can be added to database

--- companyRouter.js
+++ companyRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 var router = express.Router();
 
 router.get('/', (req,res,next) => {
@@ -8,10 +9,31 @@ router.get('/', (req,res,next) => {
 router.get('/customers', (req,res,next) => {
     res.sendFile(path.join(__dirname, 'public/customer.html'));
 });
-router.get('/customers/data', (req,res,next) => {
-    res.send(JSON.stringify({
-        text: 'hello'
-    }))
+router.post('/customers', (req,res,next) => {
+    // console.log('hello');
+    fs.readFile('companies.json', (err, content) => {
+        if(err) throw err;
+        let dataBase = JSON.parse(content);
+        let customer = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            age: req.body.age,
+            spent: req.body.spent,
+        }
+        let company = dataBase.companies.filter((company) => {
+            if(company.username === req.body.username){
+                return company;
+            }
+        })[0];
+        let companyIndex = dataBase.companies.indexOf(company);
+        dataBase.companies[companyIndex].customers.push(customer);
+        fs.writeFile('companies.json', JSON.stringify(dataBase,null,2), err => {
+            if(err) throw err;
+        })
+        res.send(JSON.stringify(company));
+
+
+    });
 });
 router.get('/products', (req,res,next) => {
     res.sendFile(path.join(__dirname, 'public/product.html'));
